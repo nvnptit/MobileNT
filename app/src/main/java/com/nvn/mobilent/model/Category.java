cf87f908e9dde586736ac83a4d7039571d2a6fba
nhatnv77 <nhatnv77@gmail.com>
2022-03-07 23:16:58 +0700

    update

---
+++ app/src/main/java/com/nvn/mobilent/model/Category.java
@@ -0,0 +1,42 @@
+package com.nvn.mobilent.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Category {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("img")
+    @Expose
+    private String img;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+}
