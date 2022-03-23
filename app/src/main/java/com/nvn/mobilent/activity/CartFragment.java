74c2ca3791cb97e97cbbfc16dfe9b46967439dbb
nhatnv77 <nhatnv77@gmail.com>
2022-03-24 01:17:20 +0700

    update CartAdapter

--- app/src/main/java/com/nvn/mobilent/activity/CartFragment.java
+++ app/src/main/java/com/nvn/mobilent/activity/CartFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.nvn.mobilent.R;
+import com.nvn.mobilent.adapter.CartAdapter;
 
 public class CartFragment extends Fragment {
     ListView lvCart;
-    TextView tv_ThongBao, tv_Total;
+    TextView tv_NoticeCart, tv_TotalCart;
+    Button btnThanhToan, btnMuaHang;
+
+    Toolbar toolbar;
+    CartAdapter cartAdapter;
 
     public CartFragment() {
         // Required empty public constructor
@@ -22,7 +31,23 @@ public class CartFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setControl();
+    }
+
+    private void setControl() {
+        lvCart = getView().findViewById(R.id.listviewcart);
+        tv_NoticeCart = getView().findViewById(R.id.tv_noticecart);
+        tv_TotalCart = getView().findViewById(R.id.totalcart);
+        btnThanhToan = getView().findViewById(R.id.btnThanhToan);
+        btnMuaHang = getView().findViewById(R.id.btnTTMuaHang);
+        toolbar = getView().findViewById(R.id.toolbarcart);
+        cartAdapter = new CartAdapter(getContext(), R.layout.fragment_cart, HomeFragment.arrCart);
+        lvCart.setAdapter(cartAdapter);
     }
 
     @Override
