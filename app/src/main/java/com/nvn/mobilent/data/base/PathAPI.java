a0bdd6a86af927f48a95b2932111b3cc01fceaff
nvnamit <nguyenvannhat@amitgroup.vn>
2022-06-03 20:52:52 +0700

    updated

--- app/src/main/java/com/nvn/mobilent/data/base/PathAPI.java
+++ app/src/main/java/com/nvn/mobilent/data/base/PathAPI.java
@@ -1,8 +1,7 @@
 package com.nvn.mobilent.data.base;
 
 public class PathAPI {
-    //    public static final String linkAPI = "http://vnn.digital";
+//    public static final String apiKey = "";
     public static final String linkAPI = "https://api.regthink.com";
 //    public static final String linkAPI = "http://nvnptit.tk";
-    public static final String apiKey = "";
 }
--- app/src/main/java/com/nvn/mobilent/screens/forgot/PhoneFragment.java
+++ app/src/main/java/com/nvn/mobilent/screens/forgot/PhoneFragment.java
@@ -146,8 +146,6 @@ public class PhoneFragment extends Fragment {
                             }
                         }
                     }
-
-
                 });
     }
 
